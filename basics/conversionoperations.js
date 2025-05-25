00ea82af16f591e30cd6285f9f4d474f4fe0e6a3
gude pavan naren <pavannarengude@gmail.com>
2025-05-25 09:52:10 +0000

    commit changes

---
+++ basics/conversionoperations.js
@@ -0,0 +1,20 @@
+let score="287gpn"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+// "287gpn" => Nan not a number
+let isLoggedIn=""
+//if it is empty then  yu get false
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//""=>false
+//"name"=>true
+let number=2004
+let stringnumber=String(number);
+console.log(stringnumber)
+console.log(typeof(stringnumber));
+
+

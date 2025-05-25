99e32ab761823b7d5912bea4debe4a51ff9c3b21
gude pavan naren <pavannarengude@gmail.com>
2025-05-25 08:48:48 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code AS NEWER VERSION
+//cant use alert() beacaus we are using nodejs not browser
+console.log(3+3);
+//code readability should be the main priority
+console.log("Pavan")
+let name="Naren"
+let age=20
+let isLoggedIn=false
+let state;
+//number => 2 to power53 is range of number
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>different from null
+//symbol =>unique
+
+//object
+console.log(typeof null);
+//typeof null is object
+//typeof undefined is undefined
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+console.log("Pavan")
+const accountId=121004
+let accountEmail="pavannarengude@gmail.com"
+var accountpassword="12345"
+accountCity="vadodara"
+//accountId=2//not allowed
+accountEmail="naregude04@gamil.com"
+accountPassword="1212121"
+accountCity="Viajayawada"
+let accountState;
+console.log(accountId);
+/*
+prefer not use "var" because of issue in block scope and function scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
